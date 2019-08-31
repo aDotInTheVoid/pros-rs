5dab60cce47a8519a613c6eae154fac27f35d518
Nixon <nixon.emoony@gmail.com>
2019-08-31 15:44:55 +0100

    Expose c code to rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+// Copyright (c) 2019, Nixon Enraght-Moony
+// All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #![no_std]
 #![crate_type = "staticlib"]
 
 use core::panic::PanicInfo;
+
+mod pros_raw;
 
 /// This function is called on panic.
 #[cfg(target_arch = "arm")]
@@ -19,12 +27,4 @@ pub extern "C" fn addition(a: i32, b: i32) -> i32 {
 #[no_mangle]
 pub extern "C" fn subtract(a: i32, b: i32) -> i32 {
     a - b
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
---
+++ src/pros_raw.rs
@@ -0,0 +1,5 @@
+//! Raw ffi bindings to pros
+//!
+//! Items here all directly call pros functions
+
+include!(concat!(env!("OUT_DIR"), "/prg.rs"));
